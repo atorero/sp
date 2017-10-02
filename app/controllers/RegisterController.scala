2c2c194e128fe994f40c9aad460e99c97b2d824e
atorero <andrey.mikhaylov.86@gmail.com>
2017-10-02 23:22:36 +0200

    first commit

---
+++ app/controllers/RegisterController.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+import play.api.i18n._
+import play.api.i18n.MessagesApi
+import play.api.i18n.Messages.Implicits._
+import play.api.data._
+import play.api.data.Forms._
+
+import services.UserData
+
+@Singleton
+class RegisterController @Inject()(
+    cc: ControllerComponents,
+    messagesApi: MessagesApi) extends AbstractController(cc) with I18nSupport {
+
+
+  val userForm = Form(
+    mapping(
+      "name" -> text,
+      "country" -> nonEmptyText,
+      "login" -> nonEmptyText,
+      "password" -> nonEmptyText
+    )(UserData.apply)(UserData.unapply)
+  )
+
+  def form = Action { implicit request =>
+    Ok(views.html.register(userForm))
+  }
+
+  def userPost = Action { implicit request =>
+    userForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest("Failed")
+      },
+      contact => {
+        val name = contact.name
+        Ok("Hi " + name + "!")
+      }
+    )
+
+  }
+
+}
---
+++ app/services/UserData.scala
@@ -0,0 +1,12 @@
+package services
+
+
+
+case class UserData(
+                   name:    String,
+                   country: String,
+                   login:   String,
+                   password: String
+                   )
+
+
