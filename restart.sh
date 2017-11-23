aa206e973ca7f2c625bf03de998fc39284abd970
atorero <32329373+atorero@users.noreply.github.com>
2017-11-23 17:20:19 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,4 +2,6 @@ FOLDER=$(cat DEPLOY_PATH)
 PID_FILE=${FOLDER}RUNNING_PID
 kill -9 $(cat $PID_FILE) || true
 rm $PID_FILE || true
-nohup ${FOLDER}scienceprovider-$1/bin/scienceprovider -Dpidfile.path=$FOLDER -Dplay.http.secret.key=$2
+COMMAND='${FOLDER}scienceprovider-$1/bin/scienceprovider -Dpidfile.path=$FOLDER -Dplay.http.secret.key=$2'
+echo $COMMAND
+nohup $COMMAND
